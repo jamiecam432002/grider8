5919495ce83d987ccea0f0d7107d1309b564cb01
Jamie Cameron <jamiecam43@gmail.com>
2020-11-14 10:44:36 -0500

    a whole whack of changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import {
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+} from "../actions/types";
+import _ from "lodash";
+
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
